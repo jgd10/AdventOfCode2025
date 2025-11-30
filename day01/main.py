61a5184d3668f2ad71a3a69fcb990a88b4d7d240
James Derrick <james.derrick@ansys.com>
2025-11-30 22:26:21 +0000

    created day1 as prep for release

---
+++ day01/main.py
@@ -0,0 +1,20 @@
+from aoc import InputType, parse_file
+
+
+def part1():
+    data = parse_file(InputType.EXAMPLE)
+    return
+
+
+def part2():
+    data = parse_file(InputType.EXAMPLE)
+    return
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == '__main__':
+    main()
